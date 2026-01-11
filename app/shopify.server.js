b5dd9eecafd93fdf66174e9d8bc245a6463ae95d
Omni Apex Group <omniapexgroup@gmail.com>
2026-01-11 10:27:18 -0800

    Downgrade API version to match library

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -1,7 +1,7 @@
 import "@shopify/shopify-app-remix/adapters/node";
 import { shopifyApp } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import { restResources } from "@shopify/shopify-api/rest/admin/2025-04";
+import { restResources } from "@shopify/shopify-api/rest/admin/2024-01";
 import prisma from "./db.server";
 const shopify = shopifyApp({ apiKey: process.env.SHOPIFY_API_KEY, apiSecretKey: process.env.SHOPIFY_API_SECRET, scopes: process.env.SCOPES?.split(",") || ["write_products", "read_themes"], appUrl: process.env.SHOPIFY_APP_URL || process.env.HOST, authPathPrefix: "/auth", sessionStorage: new PrismaSessionStorage(prisma), restResources, webhooks: { APP_UNINSTALLED: { deliveryMethod: "http", callbackUrl: "/webhooks" } }, hooks: { afterAuth: async ({ session }) => { await shopify.registerWebhooks({ session }); } }, future: { v3_webhookAdminContext: true, v3_authenticatePublic: true } });
 export default shopify;
