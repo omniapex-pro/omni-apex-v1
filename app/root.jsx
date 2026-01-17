d1e68b601487db60d5a23303a22cb168cf23bb1c
Nathan Alan Holmes <253360248+omniapex-pro@users.noreply.github.com>
2026-01-17 12:34:41 -0800

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,12 +1,18 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-
-export default function App() {
+export const loader = async () => {
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  });
+  
+};export default function App() {
+ const { apiKey } = useLoaderData();
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+<meta name="shopify-api-key" content={apiKey} />
         <Meta />
         <Links />
       </head>
